1f6238a6855a80fda24d3fa827f4d527bbe93d51
Thinley Jigme <thinleyjigme705@gmail.com>
2023-06-23 18:45:18 +0530

    added all controllers and defined endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post '/signup', to: 'users#create'
+  post '/auth/login', to: 'sessions#create'
+  get '/auth/logout', to: 'sessions#destroy'
+  get '/todos', to: 'tasks#index'
+  post '/todos', to: 'tasks#create'
+  get '/todos/:id', to: 'tasks#show'
+  put '/todos/:id', to: 'tasks#update'
+  delete '/todos/:id', to: 'tasks#destroy'
+  post '/auth/profile', to: 'user_profiles#create'
+  get '/auth/profile', to: 'user_profiles#show'
+  put '/auth/profile', to: 'user_profiles#update'
 end
